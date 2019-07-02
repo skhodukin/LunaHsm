fc4432b4f448cdd63863b2a57f625b1bb853b270
Sergey Khodukin <sergey.khodukin@advcash.com>
2019-07-02 16:40:20 +0300

    updated to run with tomcat.Packaging war

--- src/main/java/com/example/luna/LunaApplication.java
+++ src/main/java/com/example/luna/LunaApplication.java
@@ -2,9 +2,10 @@ package com.example.luna;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LunaApplication {
+public class LunaApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LunaApplication.class, args);
