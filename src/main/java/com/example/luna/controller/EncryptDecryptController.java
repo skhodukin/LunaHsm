7e164b741e0391f4bd54dcc15f8bc30507837228
Sergey Khodukin <sergey.khodukin@advcash.com>
2019-07-02 17:30:45 +0300

    Deleted tests

--- src/main/java/com/example/luna/controller/EncryptDecryptController.java
+++ src/main/java/com/example/luna/controller/EncryptDecryptController.java
@@ -3,7 +3,10 @@ package com.example.luna.controller;
 import java.io.UnsupportedEncodingException;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.luna.domain.Response;
 import com.example.luna.exceptions.AesException;
